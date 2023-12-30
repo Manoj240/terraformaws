da1a498ebbb834f0a62d6b25774c454096801c9e
Manoj240 <nmkumar240@gmail.com>
2023-12-30 05:57:31 +0530

    added mini flak proj

---
+++ miniproject/README.md
@@ -0,0 +1,4 @@
+This is a simple mini project that demonstrates how to use post build congigs like package installations in terraform itself
+
+app.py - simple flask app
+main.tf - creates resources in AWS and deploy flask app
---
+++ miniproject/main.tf
@@ -0,0 +1,125 @@
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region.
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "nametag" {
+  default = "mkterra"
+  type = string
+}
+resource "aws_key_pair" "example" {
+  key_name   = "terraform-demo-manoj"  # Replace with your desired key name
+  public_key = file("~/.ssh/mkterrlogin.pub")  # Replace with the path to your public key file
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+
+    tags = {
+        Name = var.nametag
+    }
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true #instances launched into the subnet should be assigned a public IP address
+    tags = {
+        Name = var.nametag
+    }
+}
+
+# subnet becomes public when we internet gate way to route table and that routetable to subnet
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+
+    tags = {
+        Name = var.nametag
+    }
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+    tags = {
+        Name = var.nametag
+    }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name = var.nametag
+    }
+}
+
+resource "aws_instance" "server" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/mkterrlogin")  # Replace with the path to your private key
+    host        = self.public_ip #we can use self only in same resource block else resource.exampe.publicip
+  }
+
+  # File provisioner to copy a file from local to the remote EC2 instance
+  provisioner "file" {
+    source      = "app.py"  # Replace with the path to your local file
+    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip",  # Example package installation
+      "cd /home/ubuntu",
+      "sudo pip3 install flask",
+      "sudo python3 app.py &", #This line has to be fixed, as this is not running as a process
+    ]
+  }
+}
+
+#ssh -i ~/.ssh/mkterrlogin ubuntu@54.167.90.22 - to connect above ec2,
